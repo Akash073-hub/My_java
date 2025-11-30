b0568ebeb4a0525effa1b43a8499fde86b7e7e52
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2025-12-01 00:43:26 +0530

    Add files via upload

---
+++ Projects/Advanced_Library_Management_System/InvalidPriceException.java
@@ -0,0 +1,7 @@
+package Advanced_Library_Management_System;
+
+public class InvalidPriceException extends Exception {
+    public InvalidPriceException(String message){
+        super(message);
+    }
+}
