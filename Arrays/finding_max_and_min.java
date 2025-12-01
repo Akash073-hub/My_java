9909b5aa401a802a7caf8e253a3a21223f5ca9ea
Akash073-hub <akashramachandrabhatbca24@rvu.edu.in>
2025-12-02 02:51:20 +0530

    Arrays with loops

---
+++ Arrays/finding_max_and_min.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.System;
+
+
+public class finding_max_and_min {
+    static void main() {
+        min_max  var_3 = new min_max();
+        var_3.max_min();
+
+    }
+}
+
+class min_max{
+    int[] scores = {84, 92, 76, 90, 88, 95, 73, 89};
+    int max =0;
+    int min = Integer.MAX_VALUE;
+
+    void max_min() {
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+            if (scores[i] < min) {
+                min = scores[i];
+            }
+        }
+        System.out.println("The maximum number is: " + max);
+        System.out.println("The minimum number is: " + min);
+    }
+}
