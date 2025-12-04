430d9a0d9f265165bbc3f3251c1a8a758c219dc2
Akash073-hub <akashramachandrabhatbca24@rvu.edu.in>
2025-12-04 09:54:26 +0530

    non-returning type

---
+++ Lambda/Void.java
@@ -0,0 +1,11 @@
+public class Void {
+    static void main() {
+        the_1 var_1 = (a,b) -> System.out.println((a+b));
+        var_1.add(12,12);
+    }
+}
+
+@FunctionalInterface
+interface the_1{
+    void add(int a,int b);
+}
